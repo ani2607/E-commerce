e2875be38ad9ee30d43c9278833d80345f83416e
Aniket Sharma <aniket220603@gmail.com>
2024-01-26 16:44:44 +0530

    changed the arrow angle

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -32,7 +32,7 @@ const Slider = () => {
         <div className="flex justify-center">    
             {/* <button onClick = {handleClickPrevious} className="z-10 bg-red-100 rounded-full h-9 my-[160px] font-bold">&lt;</button>      */}
             <button onClick = {handleClickPrevious} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-12 z-1 mt-[200px]">
-                <svg className="rtl:rotate-1 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <svg className="rtl:rotate-1 w-3.5 h-3.5 ms-2 rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                 </svg>
             </button>   
